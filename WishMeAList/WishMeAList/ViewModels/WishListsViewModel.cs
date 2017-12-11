f558942093ba2e97a2235457d4fee270f04033b9
RubenStandaert <ruben.standaert.w1083@student.hogent.be>
2017-12-11 18:47:57 +0100

    Add primitive listview for wishlists

--- WishMeAList/WishMeAList/ViewModels/WishListsViewModel.cs
+++ WishMeAList/WishMeAList/ViewModels/WishListsViewModel.cs
@@ -5,17 +5,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WishMeAList.Models;
+using WishMeAList.Utils;
 
 namespace WishMeAList.ViewModels
 {
     public class WishListsViewModel : ViewModelBase
     {
         public WishList wishList { get; set; }
-        public ObservableCollection<WishList> Wishes { get; set; }
+        public ObservableCollection<WishList> WishLists { get; set; }
+        public RelayCommand AddWishListCommand { get; set; }
 
-        public WishListsViewModel(List<Models.WishList> wishLists)
+        public WishListsViewModel(List<WishList> wishLists)
         {
-            Wishes = new ObservableCollection<Models.WishList>(wishLists);
+            WishLists = new ObservableCollection<WishList>(wishLists);
+            AddWishListCommand = new RelayCommand(_ => ShowAddWishList());
+        }
+
+        private void ShowAddWishList()
+        {
+
         }
     }
 }
