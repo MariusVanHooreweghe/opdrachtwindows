6a505ad7542cec0c20542620f1aed078a52743be
RubenStandaert <ruben.standaert.w1083@student.hogent.be>
2017-12-15 13:57:27 +0100

    Remove unnecessairy bindings

--- WishMeAList/WishMeAList/ViewModels/WishListsAccessingViewModel.cs
+++ WishMeAList/WishMeAList/ViewModels/WishListsAccessingViewModel.cs
@@ -12,12 +12,6 @@ namespace WishMeAList.ViewModels
 {
     public class WishListsAccessingViewModel : ViewModelBase
     {
-        private WishList _wishList;
-        public WishList WishList
-        {
-            get { return _wishList; }
-            set { _wishList = value; RaisePropertyChanged("WishList"); }
-        }
         public ObservableCollection<WishList> WishLists { get; set; }
         private NavigatorViewModel _parent { get; set; }
 
@@ -29,7 +23,7 @@ namespace WishMeAList.ViewModels
 
         public void ListView_ItemClick(object sender, ItemClickEventArgs e)
         {
-            _wishList = (WishList)e.ClickedItem;
+            WishList _wishList = (WishList)e.ClickedItem;
             this._parent.CurrentData = new WishListAccessingViewModel(WishLists.Where(val =>
                     val.WishListID == _wishList.WishListID).SingleOrDefault(), this._parent);
         }
--- WishMeAList/WishMeAList/ViewModels/WishListsViewModel.cs
+++ WishMeAList/WishMeAList/ViewModels/WishListsViewModel.cs
@@ -13,11 +13,6 @@ namespace WishMeAList.ViewModels
 {
     public class WishListsViewModel : ViewModelBase
     {
-        private WishList _wishList;
-        public WishList WishList {
-            get {return _wishList; }
-            set {_wishList = value; RaisePropertyChanged("WishList"); }
-        }
         public ObservableCollection<WishList> WishLists { get; set; }
         public RelayCommand AddWishListCommand { get; set; }
         private NavigatorViewModel _parent { get; set; }
@@ -38,7 +33,7 @@ namespace WishMeAList.ViewModels
 
         public void ListView_ItemClick(object sender, ItemClickEventArgs e)
         {
-            _wishList = (WishList) e.ClickedItem;
+            WishList _wishList = (WishList) e.ClickedItem;
             this._parent.CurrentData = new WishListViewModel(WishLists.Where(val => 
                     val.WishListID == _wishList.WishListID).SingleOrDefault(), this._parent);
         }
