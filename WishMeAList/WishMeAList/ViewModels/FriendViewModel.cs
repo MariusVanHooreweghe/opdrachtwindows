1ff76db3cf7bc30ae404890f1bb3c78623293756
Thibault Gobert <thibault.gobert.w1869@student.hogent.be>
2017-12-17 22:57:14 +0100

    added wishlists from friend

--- WishMeAList/WishMeAList/ViewModels/FriendViewModel.cs
+++ WishMeAList/WishMeAList/ViewModels/FriendViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using WishMeAList.Models;
@@ -14,15 +18,71 @@ namespace WishMeAList.ViewModels
         public WishList SelectedWishList { get; set; }
         public RelayCommand RequestAccessCommand { get; set; }
 
+        private ObservableCollection<WishList> _wishLists;
+        public ObservableCollection<WishList> WishLists {
+            get { return _wishLists; }
+            set { _wishLists = value; RaisePropertyChanged("WishLists"); }
+        }
+
         public FriendViewModel(User friend)
         {
             Friend = friend;
+            GetUser();
+        
             RequestAccessCommand = new RelayCommand(_ => RequestAccess());
         }
 
         private void RequestAccess()
         {
-            Friend.Notifications.Add(new Notification(UserManager.CurrentUser, Friend, NotificationType.REQUEST_FOR_ACCESS, DateTime.Now, SelectedWishList));
+            NotifyFriend();
+        }
+
+        private async Task GetUser()
+        {
+            HttpClient client = new HttpClient();
+            var json = await client.GetStringAsync(new Uri("http://localhost:65172/api/WishLists/user/" + Friend.UserID));
+            ObservableCollection<WishList> WishLists = JsonConvert.DeserializeObject<ObservableCollection<WishList>>(json);
+
+            Friend.WishListsOwning = WishLists;
+            this.WishLists = new ObservableCollection<WishList>(Friend.WishListsOwning);
+            
+            RaisePropertyChanged("WishLists");
+        }
+
+        private async Task NotifyFriend()
+        {
+            HttpClient client = new HttpClient();
+
+            Notification notification = new Notification(UserManager.CurrentUser, Friend, NotificationType.REQUEST_FOR_ACCESS, DateTime.Now, SelectedWishList);
+            try
+            {
+                var notificationJson = JsonConvert.SerializeObject(notification,
+                    new JsonSerializerSettings()
+                    {
+                        ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore,
+                    });
+                Debug.WriteLine(notificationJson);
+                var resNotification =
+             await client.PostAsync("http://localhost:65172/api/Notifications/", new StringContent(notificationJson, System.Text.Encoding.UTF8, "application/json"));
+                Debug.WriteLine(resNotification);
+                if (resNotification.Content != null)
+                {
+                    string newNotificationJson = await resNotification.Content.ReadAsStringAsync();
+                    if (Friend.Notifications == null)
+                    {
+                        Friend.Notifications = new Collection<Notification>();
+                    }
+                    var addedNotification = JsonConvert.DeserializeObject<Notification>(newNotificationJson);
+                    addedNotification.Sender = UserManager.CurrentUser;
+                    addedNotification.Reciever = Friend;
+                    Friend.Notifications.Add(addedNotification);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Write(e.Message);
+            }
+
         }
     }
 }
